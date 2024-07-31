f5f8caa52a96e9c16aaeae62c5a89fcc1af1d979
Alex Birch <Birch-san@users.noreply.github.com>
2024-07-31 02:41:54 +0100

    progress on image loading

--- script/dino_play.py
+++ script/dino_play.py
@@ -2,6 +2,10 @@ import argparse
 from pathlib import Path
 from dataclasses import dataclass
 from tqdm import tqdm
+import torch
+from torch import ByteTensor, FloatTensor
+from torchvision.io.image import ImageReadMode, read_image, decode_jpeg, read_file
+from torchvision.transforms.v2 import Resize, CenterCrop, Compose, Normalize
 from dino_embed.chunk import chunk
 
 @dataclass
@@ -9,11 +13,24 @@ class Args:
     in_dir: Path
     out_dir: Path
 
-img_suffices = {'.jpg', '.jpeg', '.png'}
+jpeg_suffices = {'.jpg', '.jpeg'}
+def load_image(p: Path, device=torch.device('cpu')) -> None:
+    if p.suffix in jpeg_suffices and device.type == 'cuda':
+        data: ByteTensor = read_file(str(p))
+        # NOTE: we deliberately forego apply_exif_orientation, because nvjpeg doesn't support it.
+        #       instead we will, uh, hope none of these images rely on EXIF for orientation.
+        img: ByteTensor = decode_jpeg(data, mode=ImageReadMode.RGB, device=device)
+    else:
+        img: ByteTensor = read_image(str(p), mode=ImageReadMode.RGB, apply_exif_orientation=True).to(device)
+    return img
+
+img_suffices = {*jpeg_suffices, '.png'}
 def main(args: Args) -> None:
+    device=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     # we could make this lazier, but I'd rather have a progress bar
     candidates: list[Path] = [p for p in args.in_dir.rglob('**/*') if p.suffix in img_suffices]
-    for p in tqdm(candidates):
+    for batch in tqdm(chunk(candidates, 8)):
+        tensors: list[FloatTensor] = [load_image(p, device=device) for p in batch]
         pass
 
 if __name__ == '__main__':
